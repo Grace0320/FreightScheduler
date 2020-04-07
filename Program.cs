a3bcf4fe53f3a8ca4f9fb45454e4d16753a67a4a
Grace <grace.c.320@gmail.com>
2020-04-07 13:14:57 -0400

    Clean up private vs public, better ctors, use constant for airport code length.

--- Program.cs
+++ Program.cs
@@ -7,7 +7,9 @@ namespace FreightScheduler
 {
     class FlightSchedule
     {
-        public List<Flight> Schedule;
+        private const int AIRPORT_CODE_LEN = 3;
+        private List<Flight> flightSchedule;
+
         /// <summary>
         /// Load a text file with the following format into schedule:
         /// Day 1: 
@@ -20,24 +22,24 @@ namespace FreightScheduler
         /// Flight 6: Montreal(YUL) to Vancouver(YVR)
         /// </summary>
         /// <param name="filename">filepath to text file containing flight schedule</param>
-        /// <returns>0 for success, 1 for failure</returns>
-        public int Load(string filepath)
+        /// <returns>true for success, false if load fails</returns>
+        public bool Load(string filepath)
         {
             // check for file existence
             if (!File.Exists(filepath))
             {
                 Console.WriteLine("Flight schedule file does not exist.");
-                return 1;
+                return false;
             }
 
             string[] lines = File.ReadAllLines(filepath);
             if (lines.Length == 0)
             {
                 Console.WriteLine("Flight schedule file is empty.");
-                return 1;
+                return false;
             }
 
-            Schedule = new List<Flight>();
+            flightSchedule = new List<Flight>();
             int day = 0;
             foreach (string line in lines)
             {
@@ -46,7 +48,7 @@ namespace FreightScheduler
                 if (colonIdx < 0)
                 {
                     Console.WriteLine("Flight schedule file has wrong format.");
-                    return 1;
+                    return false;
                 }
 
                 if (line.StartsWith("Day "))
@@ -59,12 +61,10 @@ namespace FreightScheduler
                     // if a day wasn't provided, can't load the schedule.
                     if (day == 0)
                     {
-                        Console.WriteLine("Flight schedule file does not exist.");
-                        return 1;
+                        Console.WriteLine("Flight schedule file has wrong format.");
+                        return false;
                     }
 
-                    Flight newFlight = new Flight();
-                    newFlight.Day = day;
                     // Note: 2 ways to go here, string ops or regex.
                     // String ops usually outperform regex except in very complicated cases.
                     // Regex makes for cleaner code. 
@@ -73,22 +73,23 @@ namespace FreightScheduler
 
                     // Get flight number
                     string flight = line.Substring(7, colonIdx - 7);
-                    newFlight.FlightNum = Int32.Parse(flight);
+                    int flightNum = Int32.Parse(flight);
 
                     // looking for instances of brackets to determine departure/arrival cities
                     // first set of brackets contains departure, second contains arrival
                     int firstOpenBracket = line.IndexOf('(');
                     int secondOpenBracket = line.IndexOf('(', firstOpenBracket + 1);
-                    newFlight.Departure = line.Substring(firstOpenBracket + 1, line.IndexOf(')', firstOpenBracket) - firstOpenBracket - 1);
-                    newFlight.Destination = line.Substring(secondOpenBracket + 1, line.IndexOf(')', secondOpenBracket) - secondOpenBracket - 1);
+                    string departure = line.Substring(firstOpenBracket + 1, AIRPORT_CODE_LEN);
+                    string destination = line.Substring(secondOpenBracket + 1, AIRPORT_CODE_LEN);
 
                     //add to schedule.
-                    Schedule.Add(newFlight);
+
+                    flightSchedule.Add(new Flight(flightNum, departure, destination, day));
 
                 }
             }
 
-            return 0;
+            return true;
         }
 
 
@@ -100,9 +101,15 @@ namespace FreightScheduler
         /// </summary>
         public void Display()
         {
-            foreach (Flight fl in Schedule)
+            if (flightSchedule.Count == 0)
             {
-                Console.WriteLine("Flight: {0}, departure: {1}, arrival: {2}, day: {3}", fl.FlightNum, fl.Departure, fl.Destination, fl.Day);
+                Console.WriteLine("There are no flights to display.");
+                return;
+            }
+
+            foreach (Flight fl in flightSchedule)
+            {
+                fl.PrintToConsole();
             }
         }
 
@@ -113,7 +120,7 @@ namespace FreightScheduler
         /// <returns>Next flight with room, if available, if not, null.</returns>
         public Flight GetNextAvailableFlight(string destination)
         {
-            foreach (Flight fl in Schedule)
+            foreach (Flight fl in flightSchedule)
             {
                 if (fl.Destination == destination && fl.HasRoom())
                     return fl;
@@ -127,26 +134,108 @@ namespace FreightScheduler
     class Flight
     {
         private const int CAPACITY = 20;
-        public int FlightNum = -1;
-        public string Departure = null;
-        public string Destination = null;
-        public int Day = -1;
-        public int ScheduledOrderNum = 0;
+        private int flightNum = -1;
+        private string departure = null;
+        private string destination = null;
+        private int day = -1;
+        private int scheduledOrderNum = 0;
 
-        public bool HasRoom()
+        public string Destination
         {
-            return ScheduledOrderNum < CAPACITY;
+            get { return destination; }
         }
 
+        public int FlightNum
+        {
+            get { return flightNum; }
+        }
+
+        public int Day
+        {
+            get { return day; }
+        }
+
+        public Flight(int _flightNum, string _departure, string _destination, int _day)
+        {
+            flightNum = _flightNum;
+            departure = _departure;
+            destination = _destination;
+            day = _day;
+        }
+
+        /// <summary>
+        /// Check if this flight is under capacity
+        /// </summary>
+        /// <returns>true if under capacity, false if at capacity</returns>
+        public bool HasRoom()
+        {
+            return scheduledOrderNum < CAPACITY;
+        }
+
+        /// <summary>
+        /// Increments the scheduledOrders if there is capacity
+        /// </summary>
+        /// <returns>true if order was added, false if not</returns>
+        public bool AddOrder()
+        {
+            if (HasRoom())
+            {
+                scheduledOrderNum++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void PrintToConsole()
+        {
+            Console.WriteLine("Flight: {0}, departure: {1}, arrival: {2}, day: {3}", flightNum, departure, destination, day);
+        }
 
     }
 
     class Order
     {
-        public string orderID = null;
-        public string departure = "YUL";  //this exercise has all orders leaving from Montreal
-        public string destination = null;
-        public Flight schedFlight = null; //use null to denote unscheduled
+        private string orderID;
+        private string departure = "YUL";  //this exercise has all orders leaving from Montreal, this will be the default departure
+        private string destination = null;
+        private Flight schedFlight = null; //use null to denote unscheduled
+
+        public string OrderID
+        {
+            get { return orderID; }
+            set { orderID = value; }
+        }
+        public string Departure
+        {
+            get { return departure; }
+            set { departure = value; }
+        }
+
+        public string Destination
+        {
+            get { return destination; }
+            set { destination = value; }
+        }
+
+        public Flight SchedFlight
+        {
+            get { return schedFlight; }
+            set { schedFlight = value; }
+        }
+
+        public void PrintToConsole()
+        {
+            if (SchedFlight == null)  //unscheduled
+            {
+                Console.WriteLine("order: {0}, flightNumber: not scheduled", orderID);
+            }
+            else
+            {
+                Console.WriteLine("order: {0}, flightNumber: {1}, departure: {2}, arrival: {3}, day: {4}",
+                    orderID, schedFlight.FlightNum, departure, destination, schedFlight.Day);
+            }
+        }
     }
 
     class OrderScheduler
@@ -167,29 +256,29 @@ namespace FreightScheduler
             //orders are stored in priority order, so just iterate through them
             foreach (string orderID in orderDict.Keys)
             {
-                Flight fl = flightSched.GetNextAvailableFlight(orderDict[orderID].destination);
+                Flight fl = flightSched.GetNextAvailableFlight(orderDict[orderID].Destination);
                 if (fl != null)
                 {
-                    orderDict[orderID].schedFlight = fl;
-                    fl.ScheduledOrderNum++;
+                    orderDict[orderID].SchedFlight = fl;
+                    fl.AddOrder();
                 }
             }
         }
 
+        /// <summary>
+        /// Display all orders' status
+        /// </summary>
         public void Display()
         {
+            if (orderDict.Count == 0)
+            {
+                Console.WriteLine("There are no orders to display");
+                return;
+            }
+            
             foreach (string orderID in orderDict.Keys)
             {
-                Order order = orderDict[orderID];
-                if (order.schedFlight == null)  //unscheduled
-                {
-                    Console.WriteLine("order: {0}, flightNumber: not scheduled", orderID);
-                }
-                else
-                {
-                    Console.WriteLine("order: {0}, flightNumber: {1}, departure: {2}, arrival: {3}, day: {4}",
-                        orderID, order.schedFlight.FlightNum, order.departure, order.destination, order.schedFlight.Day);
-                }
+                orderDict[orderID].PrintToConsole();
             }
         }
     }
@@ -198,7 +287,7 @@ namespace FreightScheduler
         static void Main(string[] args)
         {
             FlightSchedule flightSched = new FlightSchedule();
-            if (flightSched.Load(args[0]) == 0)
+            if (flightSched.Load(args[0]))
             {
                 flightSched.Display();
 
@@ -206,9 +295,16 @@ namespace FreightScheduler
                 string json = File.ReadAllText(args[1]);
                 Dictionary<string, Order> orders = JsonConvert.DeserializeObject<Dictionary<string, Order>>(json);
 
+                //add order IDs to objects - the given json uses the order ID as a dynamic key, so it can't be deserialized into the class directly.
+                foreach (string orderID in orders.Keys)
+                {
+                    orders[orderID].OrderID = orderID;
+                }
+
                 OrderScheduler orderScheduler = new OrderScheduler(flightSched, orders);
                 orderScheduler.Schedule();
                 orderScheduler.Display();
+
             }
         }
     }
